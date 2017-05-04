05b9e8cd7969b8c486d3aa85f50f15d0903cae97
sharon mcdermott <sharonmcdermott@me.com>
2017-05-04 13:46:05 -0400

    Added some classes that might be used to help track the players turns in the game. These classes may be converted into protocols and structs, but I may be trying to get too advanced for myself right now.

--- TicTacShake/ViewController.swift
+++ TicTacShake/ViewController.swift
@@ -23,3 +23,77 @@ class ViewController: UIViewController {
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+// How do I use the Minimax Algorithm in my game to pick the game piece placement for the opponent? (shaken game piece placement.)
+
+
+
+
+// from WWDC 2016 Session 404 Developer Tools from the third instructor at minute 44:50 on the video / This may help you keep track of the players turns in the game.
+// it starts off using a super classes and sub classes
+// you may want to delv deeper and use protocols and structs
+
+// How do I define the board?
+
+/*
+protocol Player {
+    func takeTurn(/* on board: Board */)
+}
+// you'll need to define the board once you get the individual places for the pieces added
+
+
+struct HumanPlayer: Player {
+    var name: String
+    var score: Int
+    func takeTurn(/* on board: Board */) { /* Show move UI and wait */ }
+    }
+
+
+struct ComputerPlayer: Player {
+    func takeTurn(/* on board: Board */) { /*Pick the best legal move using AI */}
+}
+
+
+let player = HumanPlayer(name: "Lynn", score: 0)
+//print(player)
+
+*/
+
+/*
+
+protocol CustomStringConvertible {
+    var description: String { get }
+}
+
+struct HumanPlayer: Player {...}
+
+extension HumanPlayer: CustomStringConvertible {
+    var description: String {
+        return "Human player \(name) has a score of \(score)"
+    }
+}
+
+
+let player = HumanPlayer(name: "Lynn", score: 0)
+print(player)
+*/
+
+
+
+
+
+
+
+
+
